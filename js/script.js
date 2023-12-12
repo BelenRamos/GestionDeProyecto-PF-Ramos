ab33cf2701d68c2bc43f5bcaab57736dbdacd4b2
Belen Ramos <m.b.ramos0204@gmail.com>
2023-12-12 10:52:54 -0300

    Agrego Libreria SweetAlert

--- js/script.js
+++ js/script.js
@@ -140,107 +140,219 @@ function Alumno(nombre, apellido, edad, genero) {
 }
 
 function agregarAlumno() {
-  let nombre = prompt("Ingrese el NOMBRE del alumno (Sin espacios ni símbolos):");
-  let esNombre = containsOnlyLetters(nombre);
-  
-  if (!esNombre) {
-    console.log("Error: El nombre debe contener solo letras");
-    return;
-  }
+  Swal.fire({
+    title: 'Ingrese los datos del alumno',
+    html:
+      '<input id="nombre" class="swal2-input" placeholder="Nombre">' +
+      '<input id="apellido" class="swal2-input" placeholder="Apellido">' +
+      '<input id="genero" class="swal2-input" placeholder="Género (F/M)">' +
+      '<input id="edad" type="number" class="swal2-input" placeholder="Edad">',
+    focusConfirm: false,
+    preConfirm: () => {
+      const nombre = Swal.getPopup().querySelector('#nombre').value;
+      const apellido = Swal.getPopup().querySelector('#apellido').value;
+      const genero = Swal.getPopup().querySelector('#genero').value;
+      const edad = Swal.getPopup().querySelector('#edad').value;
 
-  let apellido = prompt("Ingrese el APELLIDO del alumno (Sin espacios ni símbolos):");
-  let esApellido = containsOnlyLetters(apellido);
-  
-  if (!esApellido) {
-    console.log("Error: El apellido debe contener solo letras");
-    return;
-  }
+      // Validaciones
+      if (!containsOnlyLetters(nombre) || !containsOnlyLetters(apellido)) {
+        Swal.fire({
+          icon: 'error',
+          title: 'Error',
+          text: 'El nombre y el apellido deben contener solo letras.',
+        });
+        return false;
+      }
 
-  let genero = prompt("Ingrese el género del alumno: (F/M)");
-  if (genero.toLowerCase() === 'f' || genero.toLowerCase() === 'm') {
-    genero = genero.toUpperCase(); // Convertir a mayúscula
-    genero = genero === 'F' ? 'Femenino' : 'Masculino';
-    console.log(`El género es: ${genero}`);
-  } else {
-    console.log("Error: El género debe ser 'F' o 'M'.");
-    return;
-  }
+      if (!(genero.toLowerCase() === 'f' || genero.toLowerCase() === 'm')) {
+        Swal.fire({
+          icon: 'error',
+          title: 'Error',
+          text: 'El género debe ser "F" o "M".',
+        });
+        return false;
+      }
 
-  let edad = parseInt(prompt("Ingrese la edad del alumno:"));
-  if (typeof edad !== 'number' || isNaN(edad)) {
-    console.log("Error: La edad debe ser un número.");
-    return;
-  }
+      if (isNaN(parseInt(edad))) {
+        Swal.fire({
+          icon: 'error',
+          title: 'Error',
+          text: 'La edad debe ser un número.',
+        });
+        return false;
+      }
 
-  const alumnoExistente = alumnos.find(Alumno => Alumno.nombre === nombre && Alumno.apellido === apellido);
-  if (!alumnoExistente) {
-    const nuevoAlumno = new Alumno(nombre, apellido, edad, genero);
+      let generoAlumno = genero.toUpperCase() === 'F' ? 'Femenino' : 'Masculino';
 
-    alumnos.push(nuevoAlumno);
-    console.log("¡Alumno agregado exitosamente!");
-    guardarAlumnosEnStorage();
-    mostrarListaAlumnos();
-  } else {
-    console.log("¡El alumno ya existe!");
-  }
+      return {
+        nombre: nombre,
+        apellido: apellido,
+        genero: generoAlumno,
+        edad: parseInt(edad),
+      };
+    },
+  }).then((result) => {
+    if (result.isConfirmed) {
+      const { nombre, apellido, genero, edad } = result.value;
+      const alumnoExistente = alumnos.find(
+        (Alumno) => Alumno.nombre === nombre && Alumno.apellido === apellido
+      );
+
+      if (!alumnoExistente) {
+        const nuevoAlumno = new Alumno(nombre, apellido, edad, genero);
+        alumnos.push(nuevoAlumno);
+
+        Swal.fire({
+          icon: 'success',
+          title: 'Éxito',
+          text: '¡Alumno agregado exitosamente!',
+        });
+        guardarAlumnosEnStorage();
+        mostrarListaAlumnos();
+      } else {
+        Swal.fire({
+          icon: 'warning',
+          title: 'Advertencia',
+          text: '¡El alumno ya existe!',
+        });
+      }
+    }
+  });
 }
 
 function eliminarAlumno(nombre, apellido) {
-  const confirmar = confirm(`¿Estás seguro de eliminar al alumno ${nombre} ${apellido}?`);
-  if (confirmar) {
-    alumnos = alumnos.filter(Alumno => Alumno.nombre !== nombre || Alumno.apellido !== apellido);
-    guardarAlumnosEnStorage();
-    mostrarListaAlumnos(); // Mostrar la lista actualizada después de eliminar
-    console.log(`Alumno ${nombre} ${apellido} eliminado con éxito.`);
-  }
+  Swal.fire({
+    title: `¿Estás seguro de eliminar al alumno ${nombre} ${apellido}?`,
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonColor: '#d33',
+    cancelButtonColor: '#3085d6',
+    confirmButtonText: 'Sí, eliminar',
+    cancelButtonText: 'Cancelar',
+  }).then((result) => {
+    if (result.isConfirmed) {
+      alumnos = alumnos.filter((Alumno) => Alumno.nombre !== nombre || Alumno.apellido !== apellido);
+      guardarAlumnosEnStorage();
+      mostrarListaAlumnos();
+      Swal.fire({
+        icon: 'success',
+        title: 'Éxito',
+        text: `Alumno ${nombre} ${apellido} eliminado con éxito.`,
+      });
+    }
+  });
 }
 
 function actualizarAlumno(nombre, apellido) {
-  let esPalabra = containsOnlyLetters(nombre) && containsOnlyLetters(apellido);
-  if (!esPalabra) {
-    console.log("Error: El nombre y el apellido deben contener solo letras");
-    return;
-  }
+  Swal.fire({
+    title: 'Ingrese el nuevo nombre:',
+    input: 'text',
+    inputValue: nombre,
+    showCancelButton: true,
+    confirmButtonText: 'Guardar',
+    cancelButtonText: 'Cancelar',
+    inputValidator: (value) => {
+      if (!value || !containsOnlyLetters(value)) {
+        return 'El nombre debe contener solo letras';
+      }
+    }
+  }).then((result) => {
+    if (result.isConfirmed) {
+      const nuevoNombre = result.value;
 
-  let genero = prompt("Ingrese el género del alumno: (F/M)");
-  if (genero.toLowerCase() === 'f' || genero.toLowerCase() === 'm') {
-    genero = genero.toUpperCase(); // Convertir a mayúscula
-    genero = genero === 'F' ? 'Femenino' : 'Masculino';
-    console.log(`El género es: ${genero}`);
-  } else {
-    console.log("Error: El género debe ser 'F' o 'M'.");
-    return;
-  }
+      Swal.fire({
+        title: 'Ingrese el nuevo apellido:',
+        input: 'text',
+        inputValue: apellido,
+        showCancelButton: true,
+        confirmButtonText: 'Guardar',
+        cancelButtonText: 'Cancelar',
+        inputValidator: (value) => {
+          if (!value || !containsOnlyLetters(value)) {
+            return 'El apellido debe contener solo letras';
+          }
+        }
+      }).then((result) => {
+        if (result.isConfirmed) {
+          const nuevoApellido = result.value;
 
-  let edad = parseInt(prompt("Ingrese la edad del alumno:"));
-  if (typeof edad !== 'number' || isNaN(edad)) {
-    console.log("Error: La edad debe ser un número.");
-    return;
-  }
+          Swal.fire({
+            title: 'Ingrese el género del alumno: (F/M)',
+            input: 'text',
+            showCancelButton: true,
+            confirmButtonText: 'Guardar',
+            cancelButtonText: 'Cancelar',
+            inputValidator: (value) => {
+              if (!value || (value.toLowerCase() !== 'f' && value.toLowerCase() !== 'm')) {
+                return 'El género debe ser "F" o "M"';
+              }
+            }
+          }).then((result) => {
+            if (result.isConfirmed) {
+              let genero = result.value.toUpperCase(); // Convertir a mayúscula
+              genero = genero === 'F' ? 'Femenino' : 'Masculino';
+              console.log(`El género es: ${genero}`);
 
-  const alumnoActualizado = {
-    nombre: nombre,
-    apellido: apellido,
-    edad: edad,
-    genero: genero,
-    asistencia: alumnos.find(Alumno => Alumno.nombre === nombre && Alumno.apellido === apellido)?.asistencia ?? [0]
-  };
+              Swal.fire({
+                title: 'Ingrese la edad del alumno:',
+                input: 'number',
+                inputAttributes: {
+                  min: 1
+                },
+                showCancelButton: true,
+                confirmButtonText: 'Guardar',
+                cancelButtonText: 'Cancelar',
+                inputValidator: (value) => {
+                  if (!value || isNaN(value) || parseInt(value) <= 0) {
+                    return 'La edad debe ser un número mayor que cero.';
+                  }
+                }
+              }).then((result) => {
+                if (result.isConfirmed) {
+                  const edad = parseInt(result.value);
 
-  const index = alumnos.findIndex(Alumno => Alumno.nombre === nombre && Alumno.apellido === apellido);
-  if (index !== -1) {
-    // Actualizar los datos del alumno utilizando spread operator
-    alumnos = [
-      ...alumnos.slice(0, index),
-      alumnoActualizado,
-      ...alumnos.slice(index + 1)
-    ];
-    guardarAlumnosEnStorage();
-    mostrarListaAlumnos(); 
-    console.log(`Alumno ${nombre} ${apellido} actualizado con éxito.`);
-  } else {
-    console.log(`Error: No se encontró al alumno ${nombre} ${apellido}.`);
-  }
+                  const alumnoActualizado = {
+                    nombre: nuevoNombre,
+                    apellido: nuevoApellido,
+                    edad: edad,
+                    genero: genero,
+                    asistencia: alumnos.find(Alumno => Alumno.nombre === nombre && Alumno.apellido === apellido)?.asistencia ?? [0]
+                  };
+
+                  const index = alumnos.findIndex(Alumno => Alumno.nombre === nombre && Alumno.apellido === apellido);
+                  if (index !== -1) {
+                    // Actualizar los datos del alumno utilizando spread operator
+                    alumnos = [
+                      ...alumnos.slice(0, index),
+                      alumnoActualizado,
+                      ...alumnos.slice(index + 1)
+                    ];
+                    guardarAlumnosEnStorage();
+                    mostrarListaAlumnos();
+                    Swal.fire({
+                      icon: 'success',
+                      title: 'Éxito',
+                      text: `Alumno ${nombre} ${apellido} actualizado con éxito.`,
+                    });
+                  } else {
+                    Swal.fire({
+                      icon: 'error',
+                      title: 'Error',
+                      text: `No se encontró al alumno ${nombre} ${apellido}.`,
+                    });
+                  }
+                }
+              });
+            }
+          });
+        }
+      });
+    }
+  });
 }
+
+
+
 
 function cargarAsistencia(nombre, apellido, presente) {
   let esPalabra = containsOnlyLetters(nombre) && containsOnlyLetters(apellido);
