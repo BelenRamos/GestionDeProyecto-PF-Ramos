858657b138ad894bb341650697806a511f6305e8
Belen Ramos <m.b.ramos0204@gmail.com>
2023-12-16 17:21:18 -0300

    Creacion de JSON local

---
+++ js/personalS.js
@@ -0,0 +1,29 @@
+// Obtener los datos de profesores desde el archivo JSON local
+fetch('personal.json')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Error al obtener los datos');
+    }
+    return response.json(); // Convertir la respuesta a JSON
+  })
+  .then(data => {
+    console.log('Datos del personal de la escuela N°001 Escuela del Sol:', data);
+
+    const personalInfo = document.getElementById('personal-info');
+
+    data.forEach(persona => {
+      const info = document.createElement('div');
+      info.classList.add('col-md-6'); 
+      info.innerHTML = `
+        <h3>ID: ${persona.id}</h3>
+        <p>Nombre: ${persona.nombre}</p>
+        <p>Cargo: ${persona.cargo}</p>
+        <p>Grado: ${persona.cargo}</p>
+      `;
+      personalInfo.appendChild(info);
+    });
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
+
